8581838805d8948ae69581d2f3c9ced99ad111f4
MiguelCarcelen <miguel.carcelen28@gmail.com>
2024-11-10 19:24:34 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 import {config} from 'dotenv'
 config()
 
-export const  DB_HOST= process.env.DB_HOST || localhost
-export const DB_DATABASE=process.env.DB_DATABASE || base20242
-export const DB_USE=process.env.DB_USE || root
-export const DB_PASSWORD=process.env.DB_PASSWORD || ''
+export const  DB_HOST= process.env.DB_HOST || bgwoduztu8xjliztcii5-mysql.services.clever-cloud.com
+export const DB_DATABASE=process.env.DB_DATABASE || bgwoduztu8xjliztcii5
+export const DB_USE=process.env.DB_USE || u5t4yk2qcgcxyhtu
+export const DB_PASSWORD=process.env.DB_PASSWORD || 'C5yaJuqk9T7RIZ21bPBF'
 export const DB_PORT=process.env.DB_PORT || 3306
 export const PORT=process.env.PORT || 3000
 export const JWT_SECRET = process.env.JWT_SECRET || 'clave';
